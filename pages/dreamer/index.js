7c507f23e650b2e74b021bec8aeb5d990df80f25
KiraBelak <kalebchivas@hotmail.com>
2023-06-02 16:34:13 -0600

    changes

---
+++ pages/dreamer/index.js
@@ -0,0 +1,29 @@
+import React, {useContext, useState} from 'react';
+
+
+
+
+export default function Dreamer(){
+    const {publicKey, setPublicKey} = useContext(WalletContext);
+    const [project, setProject] = useState(null);
+    
+
+
+    
+    return(
+        <div>
+        {project ? (
+            <div>
+                <h1> 
+                    {project.name}
+                </h1>
+             
+                </div>
+        ) : (
+            <div>
+                <h1>no hay proyecto</h1>
+                </div>
+        )}
+        </div>
+    )
+}
