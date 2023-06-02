85fc9402d3f2188745179be1bf99bd9b1381a5ba
DanMoCa <dan.moca1973@gmail.com>
2023-06-02 18:46:40 -0500

    more bugfixing to dan.js

--- pages/dan.js
+++ pages/dan.js
@@ -1,28 +1,17 @@
-// // import lib/mongo_connect.js
-// // import Mongo from "/lib/mongo_connect.js"
 
-// export default async function Dan() {
+
+export default function Dan() {
     
-//     const mongo = await Mongo;
-//     const data = await mongo.connect().then((db) => {
-//         return db.collection("test").find({}).toArray();
-//     });
 
-//     return (
-//     <div className="flex w-full justify-center items-center md:py-20">
-//         <div className="grid grid-cols-1 w-full md:w-1/2 bg-gray-500 rounded-2xl p-8">
-//             <h1 className="text-xl text-white font-bold">Welcome to my world, beep bop</h1>
+    return (
+    <div className="flex w-full justify-center items-center md:py-20">
+        <div className="grid grid-cols-1 w-full md:w-1/2 bg-gray-500 rounded-2xl p-8">
+            <h1 className="text-xl text-white font-bold">Welcome to my world, beep bop</h1>
             
-//             <p className="text-white mt-4">This is a test page to see if I can connect to a MongoDB database.</p>
+            <p className="text-white mt-4">This is a test page to see if I can connect to a MongoDB database.</p>
 
-//             <p className="text-white mt-4">Here is some data from the database:</p>
-//             <ul className="text-white mt-4">
-//                 <li>{data[0].name}</li>
-//                 <li>{data[0].age}</li>
-//                 <li>{data[0].location}</li>
-//             </ul>
                         
-//         </div>
-//     </div>
-//   );
-// }
+        </div>
+    </div>
+  );
+}
