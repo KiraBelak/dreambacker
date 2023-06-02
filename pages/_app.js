5d4b4eb1c4fa7b343b04434fef12737ff524e7de
KiraBelak <kalebchivas@hotmail.com>
2023-06-01 23:30:41 -0600

    context to wallet

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import { WalletProvider } from '@/src/wallet'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <WalletProvider>
+  <Component {...pageProps} />
+  </WalletProvider>
+  )
 }
