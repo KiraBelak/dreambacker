1c793edbc08d9dd50f631ef7d63bdd9516c5923c
DanMoCa <dan.moca1973@gmail.com>
2023-06-02 18:08:24 -0500

    adding the dream REST endpoint

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -1,5 +1,4 @@
 import { MongoClient } from "mongodb";
-import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
     const { method, body, query } = req;
@@ -10,34 +9,43 @@ export default async function handler(req, res) {
     
     switch (method) {
         case "GET":
-            //obtener todos los contratos por mail
-            
-            
+            // Retrieve the user's profile with their wallet address
             {
                 const {id} = query;
                 const profile = await users.find({address:id}).toArray();
                 res.status(200).json({ profile: profile });
             }
-            
-            
-            // console.log('contrato', contrato);
             break;
         case "POST":
-            //crear una dieta
-            // console.log('body', body);
+            // Create a new profile
             {
                 const date = new Date();
                 const {id} = query;
-                const newProfile = {
+                const newProfile = { 
                     address: id,
                     nickname: body.nickname,                
                     created_at: date,
+                    updated_at: date,
                 }
     
                 const result = await users.insertOne(newProfile);
-                res.status(201).json({message:"Perfil creado", profile: result.ops});
+                res.status(201).json({message:"Profile created", profile: result.ops});
+            }            
+            break;
+        case "PUT":
+            // Update the user's profile
+            {
+                const {id} = query;
+                const updatedProfile = {
+                    nickname: body.nickname,
+                    updated_at: new Date(),
+                }
+
+                const result = await users.updateOne({address:id}, {$set: updatedProfile});
+
+                res.status(201).json({message:"Profile updated", profile: updatedProfile});
             }
-            
+
             break;
     }
     client.close();
