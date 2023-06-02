b0afe4a3f918d823a75d3a2cae3746d94b34218a
Grabiela López <105994141+grabielairu@users.noreply.github.com>
2023-06-01 22:33:13 -0600

    Update Footer.js

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-white">
+    <footer className="bg-black">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav
           className="-mx-5 -my-2 flex flex-wrap justify-center"
@@ -11,7 +11,7 @@ const Footer = () => {
           <div className="px-5 py-2">
             <Link
               href="/contacto"
-              className="text-base text-gray-500 hover:text-gray-900"
+              className="text-base text-white hover:text-white"
             >
               {" "}
               Contacto{" "}
@@ -67,8 +67,8 @@ const Footer = () => {
             </svg>
           </a>
         </div>
-        <p className="mt-8 text-center text-base text-gray-400">
-          &copy; 2022 TodoEnMinutos.Com{" "}
+        <p className="mt-8 text-center text-base text-white">
+          &copy; 2023 DreamBacker{" "}
         </p>
       </div>
     </footer>
