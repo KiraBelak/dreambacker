30593a04a04674e079a40ce37fbc6d6718ce1287
KiraBelak <kalebchivas@hotmail.com>
2023-06-01 23:30:41 -0600

    context to wallet

---
+++ src/wallet.js
@@ -0,0 +1,15 @@
+import { createContext, useState} from "react";
+
+export const WalletContext = createContext();
+
+export const WalletProvider = ({ children }) => {
+    const [publicKey, setPublicKey] = useState(null);
+    console.log("desde context");
+    console.log("wallet", publicKey);
+    
+    return (
+        <WalletContext.Provider value={{ publicKey, setPublicKey }}>
+            {children}
+        </WalletContext.Provider>
+    );
+};
