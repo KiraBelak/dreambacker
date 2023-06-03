4aad4fff5f2564ec95b9e3de60c0af182e76421a
KiraBelak <kalebchivas@hotmail.com>
2023-06-02 21:27:58 -0600

    btn wallet and dropdown and dreamer page demo

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,27 @@ import { Inter } from "next/font/google";
 import { ConnectWallet } from "../components/connectwallet";
 import MainLayout from "@/components/layouts/MainLayout";
 import MovingGallery from "@/components/MovingGallery";
-
-
-
+import { WalletContext } from "../src/wallet";
+import { useRouter } from "next/router";
+import { useContext } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { publicKey, setPublicKey } = useContext(WalletContext);
+  const router = useRouter();
+
+
+  if (publicKey) {
+    console.log("the public",publicKey);
+    router.push("/dreamer");
+  }
+  
+
+
   return (
     <div>
       <MainLayout>
-        <ConnectWallet />
         <div className="page-container">
           <div className="hero-section">
             {/* Your hero section content goes here */}
@@ -43,6 +53,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  
   );
 }
