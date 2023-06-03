842f4b9c165ef1d0610eebf9af13c8bd0793ba42
DanMoCa <dan.moca1973@gmail.com>
2023-06-03 13:58:11 -0500

    adding the edit button to dream detail and backers badges containers

--- pages/api/dream/index.js
+++ pages/api/dream/index.js
@@ -11,14 +11,20 @@ export default async function handler(req, res) {
         case "GET":
             // Retrieve the dream's information with it's id
             {   
-                const {user_id} = query;
-                if(user_id == undefined){
+                const {user_id, wallet} = query;
+                if(user_id == undefined && wallet == undefined){
                     const result = await dreams.find().toArray();
-                    res.status(200).json({ dreams: result });
-                }else{
-                    const result = await dreams.find({user_id:user_id}).toArray();
-                    res.status(200).json({ dreams: result });
                 }
+                if(wallet){
+                    const result = await dreams.find({wallet:wallet}).toArray();
+                }
+                if(user_id){
+                    const result = await dreams.find({user_id:user_id}).toArray();
+                }
+                    
+                res.status(200).json({ dreams: result });
+                
+                
             }
             break; 
         case "POST":
