c03c551b2b1a9d11a34ab06a3a18ade7e14564af
Grabiela López <105994141+grabielairu@users.noreply.github.com>
2023-06-03 03:26:35 -0600

    arreglar <img

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-black">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-        <nav className="-mx-5 -my-2 " aria-label="Footer">
+        <nav className="-mx-5 -my-2 justify-center" aria-label="Footer">
           <div className="px-5 py-2">
             <Link
               href="/contacto"
@@ -19,7 +19,7 @@ const Footer = () => {
               className="text-base text-white hover:text-gray-900"
             >
               {" "}
-              FAQ's{" "}
+              FAQs{" "}
             </Link>
             <p className="text-center text-base text-white">
               &copy; 2023 DreamBacker{" "}
--- pages/dream/create.js
+++ pages/dream/create.js
@@ -259,10 +259,7 @@ export default function Example() {
               <h2 className="text-base font-semibold leading-7 text-white">
                 Notifications
               </h2>
-              <p className="mt-1 text-sm leading-6 text-gray-400">
-                We'll always let you know about important changes, but you pick
-                what else you want to hear about.
-              </p>
+              <p className="mt-1 text-sm leading-6 text-gray-400"></p>
 
               <div className="mt-10 space-y-10">
                 <fieldset>
