ac15aa1e33c63477d531e3a567293115f56ac3c2
KiraBelak <kalebchivas@hotmail.com>
2023-06-03 04:11:29 -0600

    fix head

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { ConnectWallet } from "../ConnectWallet";
+import { ConnectWallet } from "../../ConnectWallet";
 
 const Header = () => {
   return (
