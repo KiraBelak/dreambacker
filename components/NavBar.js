08b04bfa1b95a4c66419c99a6fb487a7c293ffd5
KiraBelak <kalebchivas@hotmail.com>
2023-06-03 05:09:41 -0600

    notification elimated

--- components/NavBar.js
+++ components/NavBar.js
@@ -99,13 +99,7 @@ export default function Example() {
                   </button>
                 </div>
                 <div className="hidden md:ml-4 md:flex md:flex-shrink-0 md:items-center">
-                  <button
-                    type="button"
-                    className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    <span className="sr-only">Notificaciones</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
+                 
 
                   {/* Profile dropdown */}
                   <Menu as="div" className="relative ml-3">
