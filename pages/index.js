15d1f18ec69dc6f88f929b309143242f3771307f
Grabiela López <105994141+grabielairu@users.noreply.github.com>
2023-06-02 18:59:02 -0600

    construyendo moving gallery
    borrador en construcción de la galeria de proyectos

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import { Inter } from "next/font/google";
 import { ConnectWallet } from "../components/connectwallet";
 import MainLayout from "@/components/layouts/MainLayout";
+import MovingGallery from "@/components/MovingGallery";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <MainLayout
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <ConnectWallet />
-    </MainLayout>
+    <div>
+      <MainLayout>
+        <ConnectWallet />
+        <div>
+          <MovingGallery />
+        </div>
+      </MainLayout>
+    </div>
   );
 }
