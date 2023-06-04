be4454a86015a83c52a9c936908a0f1000f387b7
Grabiela López <105994141+grabielairu@users.noreply.github.com>
2023-06-03 22:10:31 -0600

    ignirar este commit

--- pages/terms.js
+++ pages/terms.js
@@ -3,7 +3,7 @@ import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
 
 const faqs = [
   {
-    question: "What's the best thing about Switzerland?",
+    question: "Hola?",
     answer:
       "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
   },
