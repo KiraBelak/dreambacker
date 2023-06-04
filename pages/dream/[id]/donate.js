6412a4bd27a8e2558acd0d5f0f321a193cdce392
Grabiela López <105994141+grabielairu@users.noreply.github.com>
2023-06-03 22:55:23 -0600

    cambios pag de donar, su botón y componente de wallet

--- pages/dream/[id]/donate.js
+++ pages/dream/[id]/donate.js
@@ -1,3 +1,4 @@
+import { Input } from "@/components/forms/fields";
 import { CheckIcon } from "@heroicons/react/20/solid";
 
 const includedFeatures = [
@@ -23,7 +24,7 @@ export default function Example() {
         <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
           <div className="p-8 sm:p-10 lg:flex-auto">
             <h3 className="text-2xl font-bold tracking-tight text-gray-900">
-              NFT Oro
+              🧈 NFT Oro
             </h3>
             <p className="mt-6 text-base leading-7 text-gray-600">
               Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque
@@ -58,9 +59,11 @@ export default function Example() {
                   Pay once, own it forever
                 </p>
                 <p className="mt-6 flex items-baseline justify-center gap-x-2">
-                  <span className="text-5xl font-bold tracking-tight text-gray-900">
-                    $349
-                  </span>
+                  <Input
+                    className="text-2xl font-bold text-right pr-2 text-gray-900"
+                    placeholder="000"
+                  ></Input>
+
                   <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">
                     SOL
                   </span>
